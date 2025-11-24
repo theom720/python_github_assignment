5f7cb8dc10464971ab67d7513e1843de80fdc01e
The Major <tmajor1@usf.edu>
2025-11-23 23:44:33 -0500

    Updated code

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+#Study Hours Checker
+
 print("Welcome to my Python program!")
+try:
+    
+    hours = input("How many hours did you study today? ")
+
+
+    hours = float(hours)
+
+
+except ValueError:
+    
+    print("Please enter a valid number.")
+    exit()
+
+
+weekly_hours = hours * 7
+
+print(f"You are on track to study {weekly_hours} hours this week.")
